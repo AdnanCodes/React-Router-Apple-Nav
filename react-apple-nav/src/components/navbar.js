670a289aca7ee92408359253996144d8996af1d1
Adnan <adnan.chowdhury@outlook.com>
2019-07-23 16:27:30 -0500

    Added simple navBar Component and InnerNav Component

---
+++ react-apple-nav/src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function NavBar() {
+
+    return (
+        <div>
+            <span>Mac  </span>
+            <span>iPad  </span>
+            <span>iPhone  </span>
+            <span>Watch  </span>
+            <span>TV  </span>
+            <span>Music  </span>
+        </div>
+    )
+}
